f22eb8c5dc70ad4520ed1356cddf47f7daad724a
lykrew <lykrew@gmail.com>
2026-02-05 21:27:15 +0300

    buildTree js

--- script.js
+++ script.js
@@ -8,3 +8,38 @@ function parseAndDisplayJson() {
         document.getElementById('jsonViewer').innerHTML = '<p style="color: red;">Invalid JSON</p>'
     }
 }
+
+function buildTree(obj, parentElement, key) {
+    const item = document.createElement('div');
+    parentElement.appendChild(item);
+
+    if (typeof obj === 'object' && obj !== null) {
+        const keySpan = document.createElement('span');
+        keySpan.className = 'key collapsible';
+        keySpan.textContent = key + ': ';
+        item.appendChild(keySpan);
+
+        const childContainer = document.createElement('div');
+        childContainer.className = 'hidden' + (Array.isArray(obj) ? 'array' : 'object');
+        item.appendChild(childContainer);
+
+        for (const childKey in obj) {
+            buildTree(obj[childKey], childContainer, childKey);
+        }
+
+        keySpan.onclick = function(event) {
+            event.stopPropagation();
+            const childDiv = this.parentElement.querySelector('.hidden');
+            if (childDiv.style.display === 'block') {
+                childDiv.style.display = 'none';
+                this.classList.remove('collapsed');
+            } else {
+                childDiv.style.display = 'block';
+                this.classList.add('collapsed');
+            }
+        };
+    } else {
+        item.innerHTML = '<span class="key">' + key + ': </span' + '<span class="' + getType(obj) + '">' + obj + '</span>';
+    }
+}
+
