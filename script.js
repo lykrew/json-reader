a8ba0af2cfec82da153b678ee6cc75e7b8762cb6
lykrew <lykrew@gmail.com>
2026-02-07 16:17:21 +0300

    fileInput events

--- script.js
+++ script.js
@@ -43,3 +43,26 @@ function buildTree(obj, parentElement, key) {
     }
 }
 
+function getType(value) {
+    if (typeof value === 'string') return 'string';
+    if (typeof value === 'number') return 'number';
+    if (Array.isArray(value)) return 'array';
+    if (typeof value === 'object' && value !== null)
+    return 'object';
+    return 'unknown';
+}
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (!file) {
+        return;
+    }
+
+    let reader = new FileReader();
+    reader.onload = function(event) {
+        const fileContent = event.target.result;
+        document.getElementById('jsonInput').value = fileContent;
+        parseAndDisplayJson();
+    };
+    reader.readAsText(file);
+})
